c29f7b9b76414cafa3fb1d063faa693dd3973a6c
Nimish <nimish.ie19@nsut.ac.in>
2023-09-24 00:44:12 +0530

    data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+
+@dataclass
+class DataTransformation:
+    
+
+
