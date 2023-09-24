0288b1cc4ba34169a45882dceb0df9724e306877
Nimish <nimish.ie19@nsut.ac.in>
2023-09-24 13:43:21 +0530

    Model training

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
 
 from src.components.data_transformation import DataTransformation
-
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
 import sys
 
 
@@ -11,12 +11,16 @@ if __name__=="__main__":
     logging.info('The execution has started')
 
 try:
-        
+    #daa ingestion  
     obj=DataIngestion()
     train_data_path, test_data_path =obj.initiate_data_ingestion()
-
+    #data transformation
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation( train_data_path,test_data_path)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation( train_data_path,test_data_path)
+
+    #model training
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr)) #to print R2 score
 
 except Exception as e:
     logging.info("Custom Exception")
