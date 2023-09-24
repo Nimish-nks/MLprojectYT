a4481c284b087c7c0ad465415d2648b2a26f3a94
Nimish <nimish.ie19@nsut.ac.in>
2023-09-24 11:17:03 +0530

    data transformation

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from src.logger import logging
 from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
+
+from src.components.data_transformation import DataTransformation
+
 import sys
 
 
@@ -10,7 +13,10 @@ if __name__=="__main__":
 try:
         
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path, test_data_path =obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation( train_data_path,test_data_path)
 
 except Exception as e:
     logging.info("Custom Exception")
